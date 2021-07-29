a26cd4ed313aa8ebcfd84d74775320d609183105
rakhmanhanna <rakhmanhanna@gmail.com>
2021-07-29 15:38:22 +0300

    tasks

---
+++ tasks.js
@@ -0,0 +1,30 @@
+// let admin;
+// let name;
+// name = 'Джон';
+// admin = name;
+// alert(admin);
+
+// let ourPlanet = 'Earth'
+// let userName = 'Vasia'
+
+// let yourName = prompt('Your name', 'name');
+// alert(yourName);
+
+
+
+// let name = prompt('Какое «официальное» название JavaScript?', '');
+// if (name == 'ECMAScript') {
+//     alert('Верно!');
+// } else {
+//     alert('Не знаете? ECMAScript!');
+// }
+
+
+
+// let result;
+
+// if (a + b < 4) {
+//   result = 'Мало';
+// } else {
+//   result = 'Много';
+// }
